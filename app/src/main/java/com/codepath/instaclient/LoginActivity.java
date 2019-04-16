fbe06e1cb62132ee0f33c54a1c7fd66996915fe3
mchanni1321 <Mehak.Channi76@qmail.cuny.edu>
2019-04-16 18:24:48 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/instaclient/LoginActivity.java
@@ -0,0 +1,65 @@
+package com.codepath.instaclient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username, password);
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "issue with login");
+                    e.printStackTrace();
+                    return;
+                }
+                
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+
+        Intent i = new Intent(this, MainActivity.class);
+        Log.d(TAG, "navigating to main activity");
+        startActivity(i);
+        finish();
+
+    }
+}
